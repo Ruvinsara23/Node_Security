5afc8771db88f74b04168c02a198ff2af80ef168
Pansilu Ruvinsara <ruvinsara23@gmail.com>
2024-10-20 13:22:13 +0530

    add https certification for project

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-
+const fs = require('fs')
+const https =require('https');
 const path = require('path');
 const express =require('express')
 
 const PORT=3000;
 const app = express();
+const HTTPS_PORT = 3443;
+
+app.get('/secret',(req,res)=>{
+    return res.send('Your personal secret value 43')
+})
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'))
@@ -11,4 +17,12 @@ app.get('/',(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Listning on port ${PORT}..`)
+})
+
+https.createServer({
+    key:fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+
+},app).listen(HTTPS_PORT,()=>{
+    console.log(`Listening on port ${HTTPS_PORT}..`)
 })
